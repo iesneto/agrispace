3ed16258528e3a179a3d467e5af3e131180b8a4e
Ivo Seitenfus Neto <iesneto@gmail.com>
2025-02-04 20:39:42 -0300

    Added reward on Quest data

--- Assets/_Project/Scripts/Quests/Quest.cs
+++ Assets/_Project/Scripts/Quests/Quest.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 namespace Agrispace.Quests
 {
-    [CreateAssetMenu(fileName = "New Quest", menuName = "Quest", order = 100)]
+    [CreateAssetMenu(fileName = "New Quest", menuName = "Agrispace/Quest", order = 100)]
     public class Quest : ScriptableObject
     {
         public string QuestName;
 
         public List<QuestObjective> Objectives;
+
+        public int Reward;
     }
 }
